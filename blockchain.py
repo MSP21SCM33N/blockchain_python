49504554129f7e785b4caf8ef7e79298d509d74b
Michael Neely <mneely1@hawk.iit.edu>
2019-05-25 14:58:28 -0500

    Added more functionality to my transactions function. Now stores the recipient, sender, and amount in a dictionary and adds the transaction into my new open transactions list

--- blockchain.py
+++ blockchain.py
@@ -1,9 +1,12 @@
 
 blockchain = []
+open_transactions = []
+owner = 'Max'
 
 def get_transcation_value():
-    user_input = float(input('Your transaction amount please: '))
-    return user_input
+    tx_recipient = input('Enter the recipient of the transaction: ')
+    tx_amount = float(input('Your transaction amount please: '))
+    return tx_recipient, tx_amount #Returns a tuple
 
 def get_user_choice():
     user_input = input('Your choice: ')
@@ -14,27 +17,38 @@ def get_last_item():
         return None
     return blockchain[-1]
 
-def add_transaction (trans_amount, last_transc=[1]): # default argument
-    if (last_transc == None):
-        last_transc = [1]
-    blockchain.append([last_transc,trans_amount]) #[] types casts a list
-    print(blockchain)
+def add_transaction(recipient, sender=owner, amount=1.0):
+    """
+        Arguments:
+            :sender: The sender of the coins
+            :recipient: The recipient of the coins
+            :amount: Amount of coins sent
+    """ # default argument
+    transaction = {
+        'sender': sender, 
+        'recipient': recipient, 
+        'amount': amount
+    } # Initializing a dictionary
+
+    open_transactions.append(transaction)
+    return
 
 #add_transaction (last_transc = get_last_item(), trans_amount = 1.0) keyword arguments
 
+def mine_block():
+    pass
+
 def verify_chain(): # If the first block of the new blockchain does not equal the whole value of the previous blockchain, the blockchain is invalid. 
-    block_index = 0
+    #block_index = 0
     is_valid = True
-    for blocks in blockchain:
+    for block_index in range(len(blockchain)):
         if block_index == 0:
-            block_index+=1
             continue # will skip the default  transaction and go to another iteration starting at next blocks in sequence
-        elif blocks[0] == blockchain[block_index -1]:
+        elif blockchain[block_index][0] == blockchain[block_index -1]:
             is_valid = True
         else:
             is_valid = False
             break
-        block_index+=1
     return is_valid
 
 def print_func():
@@ -42,7 +56,8 @@ def print_func():
         print('Outputing block')
         print(block)
 
-while True:
+waiting_for_input = True
+while waiting_for_input:
     print('Please choose')
     print('1: Add a new transaction value')
     print('2: Output the blockchain blocks')
@@ -51,8 +66,10 @@ while True:
     user_input = get_user_choice()
 
     if (user_input == '1'):
-        tx_amount = get_transcation_value()
-        add_transaction (tx_amount, get_last_item())
+        tx_data = get_transcation_value()
+        recipient, amount = tx_data
+        add_transaction(recipient, amount=amount) #Allows us to skip the middle argument(Sender)
+        print(open_transactions)
     elif (user_input == '2'):
         print_func()
         print(blockchain[0])
@@ -60,7 +77,7 @@ while True:
         if len(blockchain) >= 1:
             blockchain[0] = [2]
     elif(user_input == 'q'):
-        break
+        waiting_for_input = False
     else:
         print("Invalid input, choose either 1 or 2 ")
     print('Choice registered')
